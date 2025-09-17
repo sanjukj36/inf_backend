503d0d826371d600fb37fac4c766b22045142f93
Sanju K Joseph <sanju.joseph@mariapps.com>
2025-09-17 14:51:06 +0530

    fix: remove unwanted ip for ping check

--- source_code/api/util/constants.py
+++ source_code/api/util/constants.py
@@ -3,8 +3,8 @@
 HOSTS = [
     {"name": "MDC", "ip": "172.168.0.80"},
     {"name": "DAB", "ip": "192.168.1.101"},
-    {"name": "Destination host unreachable", "ip": "192.168.18.81"},
-    {"name": "yadhu", "ip": "192.168.18.99"},
-    {"name":"____", "ip": "192.168.1.81"},
-    {"name":"____", "ip": "192.0.2.123"},
+    # {"name": "Destination host unreachable", "ip": "192.168.18.81"},
+    # {"name": "yadhu", "ip": "192.168.18.99"},
+    # {"name":"____", "ip": "192.168.1.81"},
+    # {"name":"____", "ip": "192.0.2.123"},
 ]
