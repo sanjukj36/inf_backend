21782f80fa173d588a62f3eb1e630d68ab45d70e
Sanju Joseph <sanju.joseph@mariapps.com>
2026-01-19 06:24:26 -0500

    device name change to praxis

--- source_code/api/util/constants.py
+++ source_code/api/util/constants.py
@@ -2,7 +2,7 @@
 
 HOSTS = [
     {"name": "MDC", "ip": "172.168.0.80"},
-    {"name": "DAB", "ip": "192.168.1.101"},
+    {"name": "PRAXIS ", "ip": "192.168.1.101"},
     # {"name": "Destination host unreachable", "ip": "192.168.18.81"},
     # {"name": "yadhu", "ip": "192.168.18.99"},
     # {"name":"____", "ip": "192.168.1.81"},
